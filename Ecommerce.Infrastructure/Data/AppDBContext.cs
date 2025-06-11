dd486165b802ca646169b119a48b0109a466af99
fatma zayed <fatma.zayed@intalio.com>
2025-06-11 22:44:33 +0300

    add AppDBContext

---
+++ Ecommerce.Infrastructure/Data/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ecommerce.Core.Entities.Product;
+
+
+namespace Ecommerce.Infrastructure.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Photo> Photos{ get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Add any additional model configurations here
+        }
+    }
+}
