69e39e93dc4fcbc2e658de90c3920ea9f2922576
fatma zayed <fatma.zayed@intalio.com>
2025-06-11 23:49:04 +0300

    Add Scoped DbContext

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,3 +1,5 @@
+using Ecommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Ensure the InfrastructureRegistration extension method is properly referenced
+builder.Services.Register(builder.Configuration); // Replace with the correct method name if needed
 
 var app = builder.Build();
 
--- Ecommerce.Infrastructure/InfrastructureRegisteration.cs
+++ Ecommerce.Infrastructure/InfrastructureRegisteration.cs
@@ -1,12 +1,15 @@
 ﻿using Ecommerce.Core.Interfaces;
+using Ecommerce.Infrastructure.Data;
 using Ecommerce.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection; 
 
 namespace Ecommerce.Infrastructure
 {
     public static class InfrastructureRegisteration
     {
-        public static IServiceCollection Register(this IServiceCollection services)
+        public static IServiceCollection Register(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped(typeof( IGenericRepository<>), typeof(GenericRepository<>));
             //services.AddScoped< IProductRepository, ProductRepository>();
@@ -14,6 +17,9 @@ namespace Ecommerce.Infrastructure
             //services.AddScoped<IPhotoRepository, PhotoRepository>();    
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //apply db context
+            services.AddDbContext<AppDBContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("EcommerceDb")));
             return services;
         }
     }
