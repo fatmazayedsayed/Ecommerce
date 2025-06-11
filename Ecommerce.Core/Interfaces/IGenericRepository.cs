6e5a57494b8e56492dfd96946430f9d9e184bb7b
fatma zayed <fatma.zayed@intalio.com>
2025-06-11 22:37:43 +0300

    add GenericRepository , IGenericRepository

---
+++ Ecommerce.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Interfaces
+{
+   public interface IGenericRepository< TEntity> where TEntity : class
+    {
+        Task<IReadOnlyList<TEntity>> GetAllAsync();
+        Task<IReadOnlyList<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
+
+        Task<TEntity> GetByIdAsync(int id);
+        Task<TEntity> GetByIdAsync(int id, params Expression<Func<TEntity, object>>[] includes);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity); 
+        Task DeleteAsync(int id);
+     }
+    
+}
---
+++ Ecommerce.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Ecommerce.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Repositories
+{
+    public class GenericRepository <TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<TEntity>> GetAllAsync(params System.Linq.Expressions.Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id, params System.Linq.Expressions.Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
