2ab297bc1166ea6d157ac8ec8163062822b9eabc
fatma zayed <fatma.zayed@intalio.com>
2025-06-12 19:29:38 +0300

    create BaseController

---
+++ Ecommerce.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly IUnitOfWork _work;
+
+        public BaseController(IUnitOfWork work)
+        {
+            _work = work;
+        }
+    }
+}
---
+++ Ecommerce.API/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : BaseController
+    {
+        public CategoriesController(IUnitOfWork work) : base(work)
+        {
+        }
+    }
+}
